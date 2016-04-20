947ebd649a3abca9a8661181adccb91bbdccffcf
Jordi Garcia <jordigg@users.noreply.github.com>
2016-04-20 15:43:54 +0100

    Update version 1.29

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 $packageName = "rclone"
-$version = "1.27"
+$version = "1.29"
 $url = "https://github.com/ncw/rclone/releases/download/v$version/rclone-v$version-windows-386.zip"
 $url64 = "https://github.com/ncw/rclone/releases/download/v$version/rclone-v$version-windows-amd64.zip"
-
+ 
 Install-ChocolateyZipPackage $packageName "$url" "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" "$url64"
